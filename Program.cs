d796be5d80d57a025e56ae4465d02740094465ae
V121288 <kacperwilda@proton.me>
2024-10-20 22:39:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool[] zbior = new bool[100];
+        int[] indeksy = new int[100];
+
+        for (int i = 2; i < indeksy.Length; i++)
+        {
+            indeksy[i] = i;
+        }
+
+        for (int i = 2; i < 100; i++)
+        {
+            zbior[i] = true;
+        }
+        for (int i = 2; i < 10; i++)
+        {
+            if (zbior[i])
+            {
+                for (int j = 2 * i; j < 100; j++)
+                {
+                    zbior[j] = false;
+                }
+            }
+        }
+        for (int i = 2; i < 100; i++)
+        {
+            if (zbior[i])
+            {
+                Console.Write($"{indeksy[i]} ");
+            }
+        }
+    }
+}
